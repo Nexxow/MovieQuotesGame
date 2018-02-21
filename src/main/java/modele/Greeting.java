dca3f7cfeef4ab30966772b48c6772e8ca044486
Ulysse BLAINEAU <ulysse.blaineau@etu.univ-nantes.fr>
2018-02-21 13:47:24 +0100

    Poursuite de l'initialisation du projet, rajout de classes basiques pour faire des tests

---
+++ src/main/java/modele/Greeting.java
@@ -0,0 +1,23 @@
+package modele;
+
+/**
+ * Created by E155781C on 20/02/18.
+ */
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
---
+++ src/main/java/vue/Application.java
@@ -0,0 +1,15 @@
+package vue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Ulysse Blaineau on 20/02/18.
+ */
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
