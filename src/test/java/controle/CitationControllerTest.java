314d38001091bb8c2398bb89d67eaa98ec95515f
Nexxow <nexxow@gmail.com>
2018-02-22 15:34:08 +0100

    Ajout : Tests pour la citation du jour

---
+++ src/test/java/controle/CitationControllerTest.java
@@ -0,0 +1,20 @@
+package controle;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by E155781C on 22/02/18.
+ */
+public class CitationControllerTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void recuperationCitationJour() throws Exception {
+    }
+
+}
---
+++ src/test/java/modele/classes/CitationTest.java
@@ -0,0 +1,15 @@
+package modele.classes;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by E155781C on 22/02/18.
+ */
+public class CitationTest {
+    @Test
+    public void estCitationJour() throws Exception {
+    }
+
+}
