08be1d62c96df3b82a32f718c5429b7786d849f0
Ulysse BLAINEAU <ulysse.blaineau@etu.univ-nantes.fr>
2018-02-22 14:03:03 +0100

    Ajout : Compte créé avec les différents attributs et getters / setters

---
+++ src/main/java/modele/Compte.java
@@ -0,0 +1,71 @@
+package modele;
+
+/**
+ * Created by Ulysse Blaineau on 22/02/18.
+ */
+public class Compte {
+    private int score;
+    private String pseudo;
+    private String mail;
+    private String genrePrefere;
+    private String mdp;
+    private String lienAvatar;
+
+    public Compte(String pseudo, String mail, String genrePrefere, String mdp, String lienAvatar) {
+        // On initialise le score à 0
+        this.score = 0;
+        this.pseudo = pseudo;
+        this.mail = mail;
+        this.genrePrefere = genrePrefere;
+        this.mdp = mdp;
+        this.lienAvatar = lienAvatar;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public String getPseudo() {
+        return pseudo;
+    }
+
+    public void setPseudo(String pseudo) {
+        this.pseudo = pseudo;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getGenrePrefere() {
+        return genrePrefere;
+    }
+
+    public void setGenrePrefere(String genrePrefere) {
+        this.genrePrefere = genrePrefere;
+    }
+
+    public String getMdp() {
+        return mdp;
+    }
+
+    public void setMdp(String mdp) {
+        this.mdp = mdp;
+    }
+
+    public String getLienAvatar() {
+        return lienAvatar;
+    }
+
+    public void setLienAvatar(String lienAvatar) {
+        this.lienAvatar = lienAvatar;
+    }
+}
