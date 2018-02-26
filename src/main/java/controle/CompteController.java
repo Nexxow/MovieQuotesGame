4b305a7711c23b71ed0754fd12f421140dd9c1c6
Nexxow <nexxow@gmail.com>
2018-02-26 16:31:22 +0100

    MAJ : Get un compte avec un token normalement fonctionnel

--- src/main/java/controle/CompteController.java
+++ src/main/java/controle/CompteController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import static modele.bd.CompteMongo.getCompteBD;
+
 /**
  * Created by Ulysse Blaineau on 23/02/18.
  */
@@ -14,13 +16,13 @@ public class CompteController {
     // Prends un login en entrée, créer un compte puis retourne son token
     @RequestMapping("/newUser")
     public String newUser(@RequestParam(value="login", defaultValue = "Login") String login){
-        Compte compte = new Compte(login, "", "", "", "");
+        Compte compte = new Compte(login, login + "@gmail.com", "slasher", "oui", "oui");
         return compte.getToken();
     }
 
     // Prends un token en entrée pour retourner un compte
     @RequestMapping("/getUser")
     public String getUser(@RequestParam(value="token") String token){
-        return null;
+        return getCompteBD(token);
     }
 }
--- src/main/java/modele/bd/CompteMongo.java
+++ src/main/java/modele/bd/CompteMongo.java
@@ -1,10 +1,13 @@
 package modele.bd;
 
+import com.mongodb.Block;
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import modele.classes.Compte;
 import org.bson.Document;
+
+import static com.mongodb.client.model.Filters.eq;
 
 /**
  * Created by Ulysse Blaineau on 26/02/18.
@@ -21,9 +24,33 @@ public class CompteMongo {
         MongoCollection<Document> collection = database.getCollection("comptes");
 
         Document doc = new Document("token", compte.getToken()).append("pseudo", compte.getPseudo()).append("mdp", compte.getMdp()).append("mail", compte.getMail()).append("lienAvatar",
-                compte.getLienAvatar()).append("citationFav", compte.getCitationFav()).append("genrePrefere", compte.getGenrePrefere()).append("score", compte.getScore()).append("filmVote",
-                compte.getFilmVote().toString());
+                compte.getLienAvatar()).append("genrePrefere", compte.getGenrePrefere());
 
         collection.insertOne(doc);
+    }
+
+    public static String getCompteBD(String token) {
+
+        final String[] jsonDoc = new String[1];
+
+        // Creating a Mongo client
+        MongoClient mongoClient = new MongoClient( "localhost" , 27017 );
+
+        MongoDatabase database = mongoClient.getDatabase("movieQuotesGame");
+
+        MongoCollection<Document> collection = database.getCollection("comptes");
+
+        // Permet de retourner en Json ce qu'on query
+        Block<Document> returnJson = new Block<Document>() {
+            @Override
+            public void apply(final Document document) {
+                jsonDoc[0] = document.toJson();
+            }
+        };
+
+        // Recherche dans la collection le compte avec le bon token
+        collection.find(eq("token", token)).forEach(returnJson);
+
+        return jsonDoc[0];
     }
 }
