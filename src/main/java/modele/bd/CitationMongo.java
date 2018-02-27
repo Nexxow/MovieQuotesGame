b074799a9c55cc7d3c7537083f21feb9427667a8
Nexxow <nexxow@gmail.com>
2018-02-27 11:37:52 +0100

    MAJ : CitationMongo possèdes les convertisseurs, un get et un ajout

--- src/main/java/modele/bd/CitationMongo.java
+++ src/main/java/modele/bd/CitationMongo.java
@@ -6,6 +6,10 @@ import com.mongodb.client.MongoDatabase;
 import modele.classes.Citation;
 import org.bson.Document;
 
+import java.util.Date;
+
+import static com.mongodb.client.model.Filters.eq;
+
 /**
  * Created by Ulysse Blaineau on 26/02/18.
  */
@@ -23,5 +27,32 @@ public class CitationMongo {
         Document doc = new Document("citation", citation.getCitation()).append("estCitationJour", citation.estCitationJour()).append("date", citation.getDate().toString());
 
         collection.insertOne(doc);
+    }
+
+    public static Citation getCitationBD(Date date) {
+
+        Connexion co = new Connexion();
+        MongoDatabase database = co.Connexion();
+
+        MongoCollection<Document> collection = database.getCollection("citations");
+
+        // Recherche dans la collection le compte avec le bon token
+        Document doc = collection.find(eq("date", date)).first();
+
+        return mongoToJava(doc);
+    }
+
+    public static Document javaToMongo(Citation citation){
+        Document doc = new Document("citation", citation.getCitation()).append("date", citation.getDate());
+
+        return doc;
+    }
+
+    public static Citation mongoToJava(Document doc){
+
+        // Initialisation d'un objet
+        Citation citation = new Citation(doc.getString("citation"), doc.getDate("date"));
+
+        return citation;
     }
 }
