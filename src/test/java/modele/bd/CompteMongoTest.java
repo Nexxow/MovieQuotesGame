6b94ae0100c2fe2021f55c372826eb315c8ed31b
Nicolas Bourges <nicolas.bourges@etu.univ-nantes.fr>
2018-03-12 16:43:00 +0100

    ajout test bd CompteMongo

---
+++ src/test/java/modele/bd/CompteMongoTest.java
@@ -0,0 +1,35 @@
+package modele.bd;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Nicolas Bourges on 12/03/18.
+ */
+public class CompteMongoTest {
+  @Test
+  public void ajoutCompteBD() throws Exception {
+  }
+
+  @Test
+  public void majCompteBD() throws Exception {
+  }
+
+  @Test
+  public void getCompteBD() throws Exception {
+  }
+
+  @Test
+  public void getComptesBD() throws Exception {
+  }
+
+  @Test
+  public void javaToMongo() throws Exception {
+  }
+
+  @Test
+  public void mongoToJava() throws Exception {
+  }
+
+}
