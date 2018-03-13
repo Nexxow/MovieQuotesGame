368eee479bdbbbbe7578577277370588c8accdc6
Nicolas Bourges <nicolas.bourges@etu.univ-nantes.fr>
2018-03-13 13:34:32 +0100

    Ajout des classes de tests pour RecupFilms et RecupCitation

---
+++ src/test/java/metier/RecupCitationTest.java
@@ -0,0 +1,25 @@
+package metier;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Nicolas Bourges on 13/03/18.
+ */
+public class RecupCitationTest {
+
+  String urlToRead = "http://quotes.rest/qod.json";
+  RecupCitation recupCitation;
+
+  @Test
+  public void getQuote() throws Exception {
+    assertTrue("Récupération d'une citation",recupCitation.getQuote()!=null);
+  }
+
+  @Test
+  public void getFromUrl() throws Exception {
+    assertTrue("Récupération d'une citation depuis une url",recupCitation.getFromUrl(urlToRead)!=null);
+  }
+
+}
---
+++ src/test/java/metier/RecupFilmsTest.java
@@ -0,0 +1,25 @@
+package metier;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Nicolas Bourges on 13/03/18.
+ */
+public class RecupFilmsTest {
+  RecupFilms recupfilms;
+  String apiKey;
+  String urlToRead = "https://api.themoviedb.org/3/discover/movie?api_key=f426d1cd57c76ce8189d04c7d7656164&sort_by=popularity.desc";
+
+  @Test
+  public void getMovies() throws Exception {
+    assertTrue("récupération des films",recupfilms.getMovies()!=null);
+  }
+
+  @Test
+  public void getFromUrl() throws Exception {
+    assertTrue("récupération depuis l'url à lire",recupfilms.getFromUrl(urlToRead)!=null);
+  }
+
+}
