bce6affabca7a888d1699b13d9b61eb53c5f0e31
Nicolas Bourges <nicolas.bourges@etu.univ-nantes.fr>
2018-03-13 16:10:45 +0100

    Fix refacto Connexion

--- src/test/java/metier/ScoreTest.java
+++ src/test/java/metier/ScoreTest.java
@@ -1,5 +1,6 @@
 package metier;
 
+import modele.bd.Connexion;
 import modele.classes.Compte;
 import modele.classes.Film;
 import org.junit.Before;
@@ -10,30 +11,14 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
-import static metier.Score.getClassement;
-import static modele.bd.CompteMongo.ajoutCompteBD;
 import static org.junit.Assert.*;
 
 /**
  * Created by Nicolas Bourges on 13/03/18.
  */
 public class ScoreTest {
-
+Connexion co = new Connexion();
 Score score;
-  @Test
-  public void TestgetClassement() throws Exception {
-    Compte compte = new Compte("pseudo", "test", "comédie", "mdp", "urlavatar");
-    Compte compte1 = new Compte("toto", "mail", "science fiction", "mdp", "urlavatar1");
-    ArrayList<Compte> comptes = new ArrayList<>();
-    comptes.add(compte);
-    comptes.add(compte1);
-    compte.setScore(5);
-    compte1.setScore(7);
-    ajoutCompteBD(compte);
-    ajoutCompteBD(compte1);
-    assertNotEquals("Classement des comptes",comptes,getClassement());
-
-  }
 
   @Test
   public void TestcompterScore() throws Exception {
@@ -43,7 +28,7 @@ Score score;
     Date ajd = new Date();
     Film film = new Film(42, "titre", ajd, "resume", "lienImage", 5);
     compte.setFilmVote(film);
-    ajoutCompteBD(compte);
+    co.ajoutCompteBD(compte);
     score.compterScore();
     assertEquals("Classement du joueur si bonne réponse",2,compte.getScore());
 
